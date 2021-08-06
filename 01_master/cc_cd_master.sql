9f269325adf2b2a230905f5a6267011f44551c15
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2021-08-06 19:26:39 +0900

    微調整

--- 01_master/cc_cd_master.sql
+++
@@ -1,2 +0,0 @@
-USE keiki00e;
-CREATE TABLE cc_code_master (cc_code CHAR(6),parent_cc_code CHAR(6),division_code CHAR(6),cc VARCHAR(20),department VARCHAR(6),cc_abbreviated VARCHAR(8),genka_or_keihi CHAR(2),previous_cc_code CHAR(6));
