da311c54ee8e7bbb322d98779cf3f8d3f2ac99cc
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-10-12 15:02:22 +0900

    コードの修正

--- initial/03_original/LOAD_INFILE/original_analysis_detail.sql
+++ initial/03_original/LOAD_INFILE/original_analysis_detail.sql
@@ -1,37 +1,223 @@
 USE keiki00e;
 
 SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_analysis_detail\\original_analysis_detail_2101.csv'
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1901.csv'
 INTO TABLE original_analysis_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_analysis_detail\\original_analysis_detail_2102.csv'
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1902.csv'
 INTO TABLE original_analysis_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_analysis_detail\\original_analysis_detail_2103.csv'
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1903.csv'
 INTO TABLE original_analysis_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_analysis_detail\\original_analysis_detail_2104.csv'
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1904.csv'
 INTO TABLE original_analysis_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_analysis_detail\\original_analysis_detail_2105.csv'
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1905.csv'
 INTO TABLE original_analysis_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_analysis_detail\\original_analysis_detail_2106.csv'
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1906.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1907.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1908.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1909.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1910.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1911.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_1912.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2001.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2002.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2003.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2004.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2005.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2006.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2007.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2008.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2009.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2010.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2011.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2012.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2101.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2102.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2103.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2104.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2105.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2106.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2107.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2108.csv'
+INTO TABLE original_analysis_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_analysis_detail\\original_analysis_detail_2109.csv'
 INTO TABLE original_analysis_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
--- initial/03_original/LOAD_INFILE/original_sales_detail.sql
+++ initial/03_original/LOAD_INFILE/original_sales_detail.sql
@@ -1,37 +1,21 @@
 USE keiki00e;
 
 SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_sales_detail\\original_sales_detail_2101.csv'
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_sales_detail\\original_sales_detail_1901-1912.csv'
 INTO TABLE original_sales_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_sales_detail\\original_sales_detail_2102.csv'
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_sales_detail\\original_sales_detail_2001-2012.csv'
 INTO TABLE original_sales_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_sales_detail\\original_sales_detail_2103.csv'
-INTO TABLE original_sales_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
-
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_sales_detail\\original_sales_detail_2104.csv'
-INTO TABLE original_sales_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
-
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_sales_detail\\original_sales_detail_2105.csv'
-INTO TABLE original_sales_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
-
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\03_original\\original_sales_detail\\original_sales_detail_2106.csv'
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\original_sales_detail\\original_sales_detail_2101-2109.csv'
 INTO TABLE original_sales_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
