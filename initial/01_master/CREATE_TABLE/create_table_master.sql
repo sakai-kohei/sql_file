efe9cdf501aa180b2b933b6972ff3a4eb5709063
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2021-12-10 09:51:21 +0900

    CREATE TABLEのSQL文のカラムとそのデータ型の追加

--- initial/01_master/CREATE_TABLE/create_table_master.sql
+++ initial/01_master/CREATE_TABLE/create_table_master.sql
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS mst_ao_solution_code (ao_solution_code CHAR(3), ao_so
 
 CREATE TABLE IF NOT EXISTS mst_batch_code (batch_code INT, batch VARCHAR(30));
 
-CREATE TABLE IF NOT EXISTS mst_service_code (service_code CHAR(3), service VARCHAR(40), segment_abbreviated VARCHAR(4));
+CREATE TABLE IF NOT EXISTS mst_service_code (service_code CHAR(3), service VARCHAR(40), segment_abbreviated_2019 VARCHAR(4), segment_abbreviated_2020 VARCHAR(4), segment_abbreviated_2021 VARCHAR(4));
 
-CREATE TABLE IF NOT EXISTS mst_sl_code (sl_code CHAR(3), sl_no VARCHAR(2), sl VARCHAR(22), sl_class VARCHAR(8));
+CREATE TABLE IF NOT EXISTS mst_sl_code (sl_code CHAR(3), sl_no VARCHAR(2), sl VARCHAR(22), sl_class VARCHAR(8), sl_segment CHAR(3));
 
 CREATE TABLE IF NOT EXISTS mst_work_code (work_code VARCHAR(3), works VARCHAR(18));
 
