6f14e0a2d2b967c36a0a876a730dabc30c6d8d55
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-01-19 14:08:01 +0900

    【org_sales_detail】ロードするcsvファイルのファイルパスを修正

--- operational/03_original/LOAD_INFILE/org_sales_detail/original_sales_detail_2110-2112.sql
+++ operational/03_original/LOAD_INFILE/org_sales_detail/original_sales_detail_2110-2112.sql
@@ -1,8 +1,8 @@
 USE keiki00e;
 
 SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\original_sales_detail\\original_sales_detail_2110-2112.csv'
-INTO TABLE original_sales_detail
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\operational\\03_original\\org_sales_detail\\org_sales_detail_2110-2112.csv'
+INTO TABLE org_sales_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
