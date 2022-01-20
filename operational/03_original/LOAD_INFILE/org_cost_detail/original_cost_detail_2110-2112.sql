e1adaead8c8146188660fe8237d0e58512373cfa
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-01-20 13:48:57 +0900

    【org_cost_detail】データ追加用のロードSQLファイルのコードを修正

--- operational/03_original/LOAD_INFILE/org_cost_detail/original_cost_detail_2110-2112.sql
+++ operational/03_original/LOAD_INFILE/org_cost_detail/original_cost_detail_2110-2112.sql
@@ -1,7 +1,7 @@
 USE keiki00e;
 
 SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\operational\\03_original\\org_cost_detail\\org_anken_cost_2110-2112.csv'
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\operational\\03_original\\org_cost_detail\\org_cost_detail_2110-2112.csv'
 INTO TABLE org_cost_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
