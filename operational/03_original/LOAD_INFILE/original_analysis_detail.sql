e29a287e7b4c4c2ed23ebbf7aa90ade8efdea48d
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-01-20 10:59:37 +0900

    必要のないファイルの削除

--- operational/03_original/LOAD_INFILE/original_analysis_detail.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00e;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'E:\\loadfile\\initial_file\\original_analysis_detail\\original_analysis_detail_2107.csv'
-INTO TABLE original_analysis_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
