e3299261b1dd3f67f010a2cfa85cdba80331c49c
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-02-04 12:26:44 +0900

    【i*staオリジナルテーブル】load文の修正

--- initial/03_original/LOAD_INFILE/org_anken_detail_test.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00e;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_anken_detail\\org_anken_detail_test.csv'
-INTO TABLE org_anken_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
--- initial/03_original/LOAD_INFILE/org_sales_detail_test.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00e;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_sales_detail\\org_sales_detail_test.csv'
-INTO TABLE org_sales_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
