65fa5c9439dbb6d8e25ae22f1dd23918e176d8b0
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-02-04 12:25:29 +0900

    不要ファイルの削除

--- initial/03_original/LOAD_INFILE/org_anken_budget_2021.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00h;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_anken_budget_2021.csv'
-INTO TABLE org_anken_budget_2021
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
