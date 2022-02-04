00a553d16a592f774c719ca9f9dac3d98ee085cf
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-02-04 12:33:33 +0900

    ファイル名の変更

--- operational/03_original/LOAD_INFILE/org_anken_detail/original_anken_detail_2110-2112.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00e;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\operational\\03_original\\org_anken_detail\\org_anken_detail_2110-2112.csv'
-INTO TABLE org_anken_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
--- operational/03_original/LOAD_INFILE/org_cost_detail/original_cost_detail_2110-2112.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00e;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\operational\\03_original\\org_cost_detail\\org_cost_detail_2110-2112.csv'
-INTO TABLE org_cost_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
