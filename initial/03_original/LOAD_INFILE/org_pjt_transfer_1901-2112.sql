92c2534e9ab73e3b2c848dd35ed4830be18fb0b9
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-02-07 09:15:32 +0900

    【i*staオリジナルテーブル】csvファイルのパスを修正

--- initial/03_original/LOAD_INFILE/org_pjt_transfer_1901-2112.sql
+++ initial/03_original/LOAD_INFILE/org_pjt_transfer_1901-2112.sql
@@ -1,7 +1,7 @@
 USE keiki00h;
 
 SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_pjt_transfer\\org_pjt_transfer_1901-2112.csv'
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_pjt_transfer_1901-2112.csv'
 INTO TABLE org_pjt_transfer
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
