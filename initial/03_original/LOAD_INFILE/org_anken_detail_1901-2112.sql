e99201ba77b70bd51ab59bf235aa9dd33345e7bb
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-02-07 09:23:36 +0900

    【org_anken_detail】csvファイルのパスを修正

--- initial/03_original/LOAD_INFILE/org_anken_detail_1901-2112.sql
+++ initial/03_original/LOAD_INFILE/org_anken_detail_1901-2112.sql
@@ -1,7 +1,7 @@
 USE keiki00h;
 
 SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_anken_detail\\org_anken_detail_1901-2112.csv'
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_anken_detail_1901-2112.csv'
 INTO TABLE org_anken_detail
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
