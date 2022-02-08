f230ad2d88bfc4937eb3831b924fb3ed32981f29
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-02-08 17:30:11 +0900

    【create_table_master】テーブル定義書に沿って、カラムとデータ型を修正

--- initial/01_master/CREATE_TABLE/create_table_master.sql
+++ initial/01_master/CREATE_TABLE/create_table_master.sql
@@ -6,8 +6,8 @@ supervision_code CHAR(6),
 department_code CHAR(6), 
 division_code CHAR(6), 
 parent_division_code CHAR(6), 
-division VARCHAR(20), 
-division_abbreviated VARCHAR(8), 
+division VARCHAR(22), 
+division_abbreviated VARCHAR(10), 
 previous_division_code CHAR(6)
 );
 
@@ -18,19 +18,34 @@ department_cc_code CHAR(6),
 cc_code CHAR(6), 
 parent_cc_code CHAR(6), 
 division_code CHAR(6), 
-cc VARCHAR(20), 
-cc_abbreviated VARCHAR(8), 
-department VARCHAR(6), 
-genka_or_keihi CHAR(2), 
+cc VARCHAR(22), 
+cc_abbreviated VARCHAR(11), 
+genka_or_keihi VARCHAR(4), 
 previous_cc_code CHAR(6)
 );
 
-CREATE TABLE IF NOT EXISTS mst_ao_solution_code (ao_solution_code CHAR(3), ao_solution VARCHAR(22), ao_solution_abbreviated VARCHAR(22));
+CREATE TABLE IF NOT EXISTS mst_ao_solution_code (
+ao_solution_code CHAR(3), 
+ao_solution VARCHAR(26), 
+ao_solution_abbreviated VARCHAR(20)
+);
 
-CREATE TABLE IF NOT EXISTS mst_batch_code (batch_code INT, batch VARCHAR(30));
+CREATE TABLE IF NOT EXISTS mst_service_code (
+recode_year CHAR(5), 
+service_code VARCHAR(3), 
+service VARCHAR(36), 
+segment_abbreviated VARCHAR(6)
+);
 
-CREATE TABLE IF NOT EXISTS mst_service_code (service_code CHAR(3), service VARCHAR(40), segment_abbreviated_2019 VARCHAR(4), segment_abbreviated_2020 VARCHAR(4), segment_abbreviated_2021 VARCHAR(4));
+CREATE TABLE IF NOT EXISTS mst_sl_code (
+sl_code CHAR(3), 
+sl_no VARCHAR(2), 
+sl VARCHAR(23), 
+sl_class VARCHAR(10), 
+sl_segment CHAR(3)
+);
 
-CREATE TABLE IF NOT EXISTS mst_sl_code (sl_code CHAR(3), sl_no VARCHAR(2), sl VARCHAR(22), sl_class VARCHAR(8), sl_segment CHAR(3));
-
-CREATE TABLE IF NOT EXISTS mst_work_code (work_code VARCHAR(3), works VARCHAR(18));
+CREATE TABLE IF NOT EXISTS mst_work_code (
+work_code VARCHAR(3), 
+works VARCHAR(18)
+);
