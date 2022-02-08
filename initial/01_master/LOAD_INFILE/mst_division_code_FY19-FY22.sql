908990c831f248b1e8365d74d6861db1be8510bf
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-02-08 09:46:03 +0900

    【mst_cc_code】【mst_division_code】新規作成

---
+++ initial/01_master/LOAD_INFILE/mst_division_code_FY19-FY22.sql
@@ -0,0 +1,8 @@
+USE keiki00e;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\01_master\\mst_division_code_FY19-FY22.csv'
+INTO TABLE mst_division_code
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
