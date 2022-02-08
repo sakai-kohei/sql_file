00fe87ed73abce6fd8090f376d6cfc896feca2f5
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-02-08 09:44:04 +0900

    【mst_division_code】不要テーブルの削除

--- initial/01_master/LOAD_INFILE/mst_division_code_2019.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00h;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\01_master\\mst_division_code_2019.csv'
-INTO TABLE mst_division_code_2019
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
