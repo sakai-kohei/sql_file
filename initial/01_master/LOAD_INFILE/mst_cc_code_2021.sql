97365d33ef5f38f3d1fa4c57abddf03c1a764bdb
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-02-08 09:43:40 +0900

    【mst_cc_code】不要テーブルの削除

--- initial/01_master/LOAD_INFILE/mst_cc_code_2021.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00h;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\01_master\\mst_cc_code_2021.csv'
-INTO TABLE mst_cc_code_2021
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
