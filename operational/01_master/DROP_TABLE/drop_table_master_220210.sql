d0ac32845daf3a395315b7c4f2e71270b6add8c8
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-02-10 08:50:24 +0900

    【drop_table_master】マスターを削除するためのSQLファイルを新規作成

---
+++ operational/01_master/DROP_TABLE/drop_table_master_220210.sql
@@ -0,0 +1,10 @@
+USE keiki00e;
+
+drop table mst_cc_code_2019, mst_cc_code_2020, mst_cc_code_2021, mst_division_code_2019, mst_division_code_2020, mst_division_code_2021, mst_ao_solution_code, mst_service_code, mst_sl_code;
+
+
+
+
+
+
+
