932775624c464bfcfe50e65a6deb9551ec524da1
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-03-07 20:05:53 +0900

    【load_infile】load文の新規作成

---
+++ initial/03_original/LOAD_INFILE/org_partner_close_1901-2112.sql
@@ -0,0 +1,8 @@
+USE keiki00e;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_partner_close_1901-2112.csv'
+INTO TABLE org_partner_close
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
