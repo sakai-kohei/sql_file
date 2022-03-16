b2c505c7a6f5a906233877f641dbab50943aa034
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-03-16 16:43:37 +0900

    【org_anken_budjet】操作するDBを本番環境用に変更

--- initial/03_original/CREATE_TABLE/create_table_org_anken_budjet.sql
+++ initial/03_original/CREATE_TABLE/create_table_org_anken_budjet.sql
@@ -2,7 +2,7 @@ USE keiki00h;
 
 CREATE TABLE IF NOT EXISTS org_anken_budjet(
 budjet_date CHAR(6), 
-department_cc_code CHAR(6), 
+department_code CHAR(6), 
 pipeline VARCHAR(7), 
 anken VARCHAR(100), 
 sl_no VARCHAR(2), 
@@ -11,7 +11,7 @@ segment_abbreviated VARCHAR(10),
 customer VARCHAR(30), 
 work_code VARCHAR(3), 
 insider_trading VARCHAR(9), 
-budjet_ CHAR(2), 
+budjet_quarter CHAR(2), 
 supervisor_department VARCHAR(10), 
 supervisor_staff VARCHAR(20), 
 sales BIGINT, 
