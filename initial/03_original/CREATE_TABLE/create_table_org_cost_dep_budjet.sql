09a60f06e5f05cc11f0e26a9d91f64d67247ce3e
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-03-16 16:35:00 +0900

    【org_cost_dep_budjet】各カラムのデータ型の調整

--- initial/03_original/CREATE_TABLE/create_table_org_cost_dep_budjet.sql
+++ initial/03_original/CREATE_TABLE/create_table_org_cost_dep_budjet.sql
@@ -2,8 +2,8 @@ USE keiki00e;
 
 CREATE TABLE IF NOT EXISTS org_cost_dep_budjet(
 budjet_date CHAR(6), 
-department VARCHAR(11), 
-department_code CHAR(6), 
+department VARCHAR(18), 
+department_code VARCHAR(9), 
 category VARCHAR(18), 
 subject_code CHAR(5), 
 subject VARCHAR(17), 
