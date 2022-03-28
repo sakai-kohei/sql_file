50adca1fe5275ff790498b5fd0f567133af374e6
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-03-28 17:59:01 +0900

    【i*staオリジナルテーブル】不要ファイルの削除

--- operational/03_original/LOAD_INFILE/org_pjt_detail_2201.sql
+++
@@ -1,8 +0,0 @@
-USE keiki00e;
-
-SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_pjt_detail_2201.csv'
-INTO TABLE org_pjt_detail
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
