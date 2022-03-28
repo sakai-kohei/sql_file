c734999847549eb87795c8cdd43bb00883764eaa
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-03-28 17:58:20 +0900

    【i*staオリジナルテーブル】csvファイルパスの修正

--- initial/03_original/LOAD_INFILE/org_shikakari_1901-2112.sql
+++ initial/03_original/LOAD_INFILE/org_shikakari_1901-2112.sql
@@ -1,7 +1,7 @@
 USE keiki00e;
 
 SET GLOBAL local_infile=on;
-LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_shikakari\\org_shikakari_1901-2112.csv'
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\initial\\03_original\\org_shikakari_1901-2112.csv'
 INTO TABLE org_shikakari
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\r\n'
