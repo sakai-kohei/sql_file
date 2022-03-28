f15ff0202500cd9d21c3601e9fed366b76ed0478
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-03-28 17:59:37 +0900

    【i*staオリジナルテーブル】load文の新規作成

---
+++ operational/03_original/LOAD_INFILE/org_partner_open_2201-2202.sql
@@ -0,0 +1,8 @@
+USE keiki00e;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\operational\\03_original\\org_partner_open_2201-2202.csv'
+INTO TABLE org_partner_open
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
