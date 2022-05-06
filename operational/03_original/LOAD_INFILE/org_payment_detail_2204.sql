a8abeb0b083faab14d1e4c082079d01bab43b4d6
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2022-05-06 13:19:43 +0900

    【i*staオリジナルテーブル】2022年4月load文の新規作成

---
+++ operational/03_original/LOAD_INFILE/org_payment_detail_2204.sql
@@ -0,0 +1,8 @@
+USE keiki00e;
+
+SET GLOBAL local_infile=on;
+LOAD DATA LOCAL INFILE 'F:\\02_loadfile\\operational\\03_original\\org_payment_detail_2204.csv'
+INTO TABLE org_payment_detail
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
