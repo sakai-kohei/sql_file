6e12c069912e19129b51c63d2ef76bec181a45e9
sakai kohei <sakai.kohei@isid-ao.co.jp>
2022-07-14 10:59:00 +0900

    【org_expected_detail】データ型を変更するSQLファイルの新規作成

---
+++ operational/03_original/ALTER_TABLE/alter_original_220714.sql
@@ -0,0 +1,7 @@
+USE keiki00e;
+
+ALTER TABLE org_expected_detail MODIFY cloud_service_code VARCHAR(11);
+ALTER TABLE org_expected_detail MODIFY cloud_service VARCHAR(23);
+
+ALTER TABLE org_anken_detail MODIFY cloud_service_code VARCHAR(11);
+ALTER TABLE org_anken_detail MODIFY cloud_service VARCHAR(23);
